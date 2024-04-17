d18adbfacca71e31cf3b1030dc24b4d843dc3279
Menchna <armkhachatryan05@mail.ru>
2024-04-17 19:21:46 +0400

    added

---
+++ main.py
@@ -0,0 +1,53 @@
+thepref = int(input("please choose a number from 1 to 3: "))
+
+if thepref == 1:
+    Number1 = input("enter a number: ")
+    measure_of_time = input("Enter a measure of time, like weeks, days, hours etc: ")
+    made_of_transportation = input("enter a transportation vehicle: ")
+    Adjective1 = input("enter an adjective that describes a place: ")
+    Adjective2 = input("enter an adjective: ")
+    Noun1 = input("enter a noun: ")
+    Color1 = input("enter a color: ")
+    part_of_the_body1 = input("enter a part of the body: ")
+    Verb1 = input("enter a verb: ")
+    Number2 = input("enter a number: ")
+    Noun2 = input("enter a noun: ")
+    Noun3 = input("enter a noun: ")
+    Number1 = input("enter a number: ")
+    measure_of_time = input("Enter a measure of time, like weeks, days, hours etc: ")
+    Color2 = input("enter a color: ")
+    Animal2 = input("enter animals name: ")
+    Number2 = input("enter a number: ")
+    Silly_word = input("enter a silly word: ")
+    Noun2 = input("enter a noun: ")
+        
+    print("This weekend I am going camping with " + persons_name1 + ". I packed my lantern, sleeping bag, and " + Noun1 + ". I am so " + Adjective1 +  " to "+  Verb1 + " in a tent. I am" + Adjective2 + " we might see a(n) " + Animal1 + ", I hear they’re kind of dangerous. While we’re camping, we are going to hike, fish, and " + Verb2 + ". I have heard that the " + Color1 + " lake is great for " + Verb_ing + "ing" + ". Then we will " + Adverb_ly + "ly" +  "hike through the forest for " + Number1 + " " + measure_of_time + ". If I see a " + Color2 + " " + Animal2 + " while hiking, I am going to bring it home as a pet! At night we will tell " + Number2 + " " + Silly_word + "stories and roast " + Noun2 + "around the campfire!!")
+
+elif thepref == 3:
+    persons_name1 = input("enter persons name : ")
+    Adjective1 = input("enter an adjective: ")
+    Color1 = input("enter a color: ")
+    Animal1 = input("enter animals name: ")
+    place1 = input("enter place name: ")
+    Adjective2 = input("enter an adjective: ")
+    Creature1 = input("enter magical creature(Plural): ")
+    Adjective3 = input("enter an adjective: ")
+    Creature2 = input("enter magical creature(Plural): ")
+    room_in_house = input("enter room in a house: ")
+    Noun1 = input("enter a noun: ")
+    Noun2 = input("enter a noun: ")
+    Noun3 = input("enter a noun(plural): ")
+    Adjective4 = input("enter an adjective: ")
+    Noun4 = input("enter a noun(plural): ")
+    Number1 = input("enter a number: ")
+    measure_of_time = input("Enter a measure of time, like weeks, days, hours etc: ")
+    Verb_ing = input("enter a verb: ")
+    Adjective5 = input("enter an adjective: ")
+    Noun5 = input("enter a noun(plural): ")
+    
+    print("Dear " + persons_name1 + ", I am writing to you from a " + Adjective1 + " castle in an enchanted forest. I found myself here one day after going for a ride on a " + Color1 + " " + Animal1 + " in" + place1 + ". There are " + Adjective2 + " " + Creature1 + " and " + Adjective3 + " " + Creature2 + " here! In the " + room_in_house + " there is a pool full of " + Noun1 + ". I fall asleep each night on a " + Noun2 + " of " + Noun3 + " and dream of " + Adjective4 + " " + Noun4 + ". It feels as though I have lived here for " + Number1 + " " + measure_of_time + ". I hope one day you can visit, although the only way to get here now is " + Verb_ing +"ing" + " on a " + Adjective5 + " " + Noun5 + "!!")
+
+else:
+    print("You wrote the wrong number ")
+    
+    
